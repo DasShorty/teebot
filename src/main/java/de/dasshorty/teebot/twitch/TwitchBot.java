30bf1201f86c03ba352578805fa5d55b38e3fc16
shorty <ungegrieftfirma@gmail.com>
2024-01-31 12:55:35 +0100

    add message delete if streamer goes offline

--- src/main/java/de/dasshorty/teebot/twitch/TwitchBot.java
+++ src/main/java/de/dasshorty/teebot/twitch/TwitchBot.java
@@ -4,20 +4,22 @@ import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.events.ChannelGoLiveEvent;
+import com.github.twitch4j.events.ChannelGoOfflineEvent;
 import com.github.twitch4j.helix.domain.Stream;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.NewsChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.awt.*;
 import java.time.Instant;
+import java.util.HashMap;
 
 public class TwitchBot {
 
     private final TwitchDatabase twitchDatabase;
+    private final HashMap<String, Message> notifications = new HashMap<>();
     private TwitchClient client;
 
     public TwitchBot(TwitchDatabase twitchDatabase, Guild guild) {
@@ -34,6 +36,18 @@ public class TwitchBot {
                 .build();
 
         this.loadTwitchChannel();
+
+        this.client.getEventManager().onEvent(ChannelGoOfflineEvent.class, channelGoOfflineEvent -> {
+
+            String twitchName = channelGoOfflineEvent.getChannel().getName();
+            System.out.println("Stream stoppt von " + twitchName);
+
+            if (!this.notifications.containsKey(twitchName))
+                return;
+
+            this.notifications.get(twitchName).delete().queue();
+            this.notifications.remove(twitchName);
+        });
 
         this.client.getEventManager()
                 .onEvent(ChannelGoLiveEvent.class, channelGoLiveEvent -> {
@@ -55,14 +69,14 @@ public class TwitchBot {
                     }
 
                     textChannelById.sendMessage("<@&835994615399317534>")
-                                    .addEmbeds(new EmbedBuilder()
-                                            .setAuthor(twitchName, url)
-                                            .setTitle(stream.getTitle())
-                                            .setDescription(twitchName + " ist Live gegangen! Es wird heute " + stream.getGameName() + " gestreamt!")
-                                            .setImage(stream.getThumbnailUrl())
-                                            .setTimestamp(Instant.now())
-                                            .setColor(Color.decode("#a970ff"))
-                                            .build()).addActionRow(Button.link(url, "Stream anschauen")).flatMap(Message::crosspost).queue();
+                            .addEmbeds(new EmbedBuilder()
+                                    .setAuthor(twitchName, url)
+                                    .setTitle(stream.getTitle())
+                                    .setDescription(twitchName + " ist Live gegangen! Es wird heute " + stream.getGameName() + " gestreamt!")
+                                    .setImage(stream.getThumbnailUrl())
+                                    .setTimestamp(Instant.now())
+                                    .setColor(Color.decode("#a970ff"))
+                                    .build()).addActionRow(Button.link(url, "Stream anschauen")).flatMap(Message::crosspost).queue(message -> this.notifications.put(twitchName, message));
                     System.out.println(twitchName);
                 });
     }
